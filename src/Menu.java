0dfe5ec09c873b45ca8b71ebcce19ddd8efaa4bb
Anno <1320579386@qq.com>
2018-01-08 22:14:59 +0800

    0.2

--- src/Menu.java
+++ src/Menu.java
@@ -7,9 +7,9 @@ import java.util.regex.Pattern;
  * @date 2018/01/08
  */
 public class Menu {
-    Scanner scanner = new Scanner(System.in);
-    Coach coach = new Coach();
-    Truck truck = new Truck();
+    private Scanner scanner = new Scanner(System.in);
+    private Coach coach = new Coach();
+    private Truck truck = new Truck();
     private static Pattern pattern = Pattern.compile("[0-9]*");
 
     public void main() {
@@ -17,20 +17,25 @@ public class Menu {
                 "1.查看车辆信息\n" +
                 "2.确认租车\n" +
                 "3.退出系统");
-        String option = scanner.next();
-        if (option.equals("1")) {
-            browse();
-        } else if (option.equals("2")) {
-            confirm();
-        } else if (option.equals("3")) {
-            System.exit(0);
-        } else {
-            System.out.println("请输入正确的指令\n");
-            main();
+        int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                browse();
+                break;
+            case 2:
+                confirm();
+                break;
+            case 3:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("请输入正确的指令\n");
+                main();
+                break;
         }
     }
 
-    public void browse() {
+    private void browse() {
         System.out.println("客车信息\n" +
                 "价格：" + coach.price + "元/天    载客量：" + coach.people + "人/车\n" +
                 "库存" + coach.quantity + "辆\n\n" +
@@ -40,65 +45,90 @@ public class Menu {
         try {
             Thread.sleep(2000);
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
         main();
     }
 
-    public void confirm() {
+    private void confirm() {
         System.out.println("请输入车辆类型\n" +
                 "1.客车（剩余" + coach.quantity + "辆)\n" +
                 "2.货车（剩余" + truck.quantity + "辆)\n" +
                 "3.返回主菜单");
-        String option = scanner.next();
-        if (option.equals("1")) {
-            check(coach.quantity);
-        } else if (option.equals("2")) {
-            check(truck.quantity);
-        } else if (option.equals("3")) {
-            main();
-        } else {
-            System.out.println("请输入正确的指令\n");
-            confirm();
-        }
-    }
-
-    public void check(int left) {
-        System.out.println("请输入租车数量");
-        String Quantity = scanner.next();
-        if (judge(Quantity)) {
-            int quantity = Integer.valueOf(Quantity);
-            if (left >= quantity) {
-                pay(quantity);
-            } else {
-                System.out.println("车辆库存不足，请减少数量\n");
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-
+        String Quantity;
+        int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("请输入客车数量");
+                Quantity = scanner.next();
+                if (judge(Quantity)) {
+                    int quantity = Integer.valueOf(Quantity);
+                    if (coach.quantity >= quantity) {
+                        coach.quantity = coach.quantity - quantity;
+                        System.out.println("请输入租车天数(整数)");
+                        int days = scanner.nextInt();
+                        System.out.println("租车成功，共计金额" + quantity * days * coach.price + "元");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        main();
+                    } else {
+                        System.out.println("车辆库存不足，请减少数量\n");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        confirm();
+                    }
+                } else {
+                    System.out.println("请输入正整数\n");
+                    confirm();
                 }
+                break;
+            case 2:
+                System.out.println("请输入货车数量");
+                Quantity = scanner.next();
+                if (judge(Quantity)) {
+                    int quantity = Integer.valueOf(Quantity);
+                    if (truck.quantity >= quantity) {
+                        truck.quantity = truck.quantity - quantity;
+                        System.out.println("请输入租车天数(整数)");
+                        int days = scanner.nextInt();
+                        System.out.println("租车成功，共计金额" + quantity * days * truck.price + "元");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        main();
+                    } else {
+                        System.out.println("车辆库存不足，请减少数量\n");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        confirm();
+                    }
+                } else {
+                    System.out.println("请输入正整数\n");
+                    confirm();
+                }
+                break;
+            case 3:
+                main();
+                break;
+            default:
+                System.out.println("请输入正确的指令\n");
                 confirm();
-            }
-        } else {
-            System.out.println("请输入正整数\n");
-            confirm();
+                break;
         }
     }
 
-    public void pay(int amount) {
-        left = left - amount;
-        System.out.println("请输入租车天数(整数)");
-        int days = scanner.nextInt();
-        System.out.println("租车成功，共计金额" + amount * days * coach.price + "元");
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-
-        }
-        main();
-    }
-
-    public boolean judge(String str) {
+    private boolean judge(String str) {
         Matcher m = pattern.matcher(str);
         return m.matches();
     }
