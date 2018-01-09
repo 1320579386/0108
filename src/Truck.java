0e65efded501893b5e5a4823844c81c8286771ea
Anno <1320579386@qq.com>
2018-01-09 09:40:42 +0800

    1.0

--- src/Truck.java
+++ src/Truck.java
@@ -3,42 +3,7 @@
  * @date 2018/01/08
  */
 public class Truck extends Car {
-    public int id = 2;
     public int quantity = 5;
     public int weight = 5;
     public int price = 60;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    @Override
-    public int getPrice() {
-        return price;
-    }
-
-    @Override
-    public void setPrice(int price) {
-        this.price = price;
-    }
 }
